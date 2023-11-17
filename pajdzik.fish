7fb041beaf08455bd7ea9c06fe169a5c3b605922
Kamil Pajdzik <kpajdzik@flexport.com>
2023-11-16 19:37:31 -0800

    Add fish config

---
+++ pajdzik.fish
@@ -0,0 +1,10 @@
+# https://github.com/jorgebucaran/fisher/issues/755#issuecomment-1408547461
+set -e __git_plugin_initialized
+__git.init
+
+alias @ __copilot_what-the-shell
+alias @g __copilot_git-assist
+alias @gh __copilot_gh-assist
+alias code code-insiders
+
+fish_ssh_agent
