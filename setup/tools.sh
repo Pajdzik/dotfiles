0570718762fb28c2345da45320b567030cb9535e
Kamil Pajdzik <pajdziu@live.com>
2024-04-17 21:45:38 -0700

    Create tools.sh

---
+++ setup/tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if Homebrew is installed
+if ! command -v brew &>/dev/null
+then
+    echo "Homebrew not installed. Installing now..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update Homebrew and install packages
+echo "Updating Homebrew..."
+brew update
+
+echo "Installing packages..."
+brew install git
+
+echo "Installation complete."
